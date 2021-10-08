574041f475233c7b73289d81db9fff874c5f01e6
Ignacio Perez Cicero <Iperezciero@danielcastelao.org>
2021-10-08 09:54:22 +0200

    share

--- src/com/cod/ejercicio1/Ejercicio1.java
+++ src/com/cod/ejercicio1/Ejercicio1.java
@@ -3,6 +3,6 @@ package com.cod.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("share con Github");
+        System.out.println("share con Github1");
     }
 }
